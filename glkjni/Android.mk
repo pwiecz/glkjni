f286b9cf66162ecc25fd75c35b97dacd117de21a
edward.mccardell <edward.mccardell@6169d07e-6ce4-11de-b5d2-8dea7b56f9cf>
2009-07-15 01:30:17 +0000

    Android makefile

---
+++ glkjni/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := glkjni
+LOCAL_SRC_FILES := event.c fileref.c fstream.c gi_blorb.c gi_dispa.c glkjni.c \
+jcall.c latin1.c main.c mstream.c sound.c stream.c unicode.c \
+win_gfx.c win_text.c window.c
+LOCAL_CFLAGS    := -DANDROID -DANDROID_GREF
+
+include $(BUILD_STATIC_LIBRARY)
