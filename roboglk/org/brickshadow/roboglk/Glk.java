c0a6767dbae575168f78d2bc8ff25561ee317e12
edward.mccardell <edward.mccardell@6169d07e-6ce4-11de-b5d2-8dea7b56f9cf>
2009-07-15 23:59:35 +0000

    License + javadoc cleanup

--- roboglk/org/brickshadow/roboglk/Glk.java
+++ roboglk/org/brickshadow/roboglk/Glk.java
@@ -1,3 +1,20 @@
+/* This file is a part of roboglk.
+ * Copyright (c) 2009 Edward McCardell
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.brickshadow.roboglk;
 
 
@@ -47,8 +64,8 @@ public interface Glk {
     /**
      * Immediately returns an event of a certain type, or null if no
      * such events are available. The events that this function returns
-     * must be of type {@code GlkEventType.Arrange},
-     * {@link GlkEventType.Timer}, or {@link GlkEventType.SoundNotify}.
+     * must be of type {@link GlkEventType#Arrange},
+     * {@link GlkEventType#Timer}, or {@link GlkEventType#SoundNotify}.
      * <p>
      * See {@link GlkEventType} for the details
      * of the different kinds of events; see {@link GlkEventQueue} for
