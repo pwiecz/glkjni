eb9727d8b59761f2535a7bae477a9a35e7c03e5f
edward.mccardell <edward.mccardell@6169d07e-6ce4-11de-b5d2-8dea7b56f9cf>
2009-07-19 02:24:09 +0000

    New version of glkmodel

--- trunk/glkmodel/project/src/org/brickshadow/modeltest/ModelTest.java
+++ trunk/glkmodel/project/src/org/brickshadow/modeltest/ModelTest.java
@@ -6,52 +6,32 @@ import org.brickshadow.roboglk.GlkFactory;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.View;
 import android.widget.TextView;
 
 
 public class ModelTest extends Activity {
     Glk glk;
     TextView tv;
+    Thread terpThread;
     
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        initView();
-
-        glk = new Glk(tv, new Handler());
-        
-        startInterpreter();
-    }
-    
-    private void initView() {
-        //setContentView(R.layout.main);
-        //tv = (TextView) findViewById(R.id.body);
-        tv = new TextView(this);
-        
-        tv.setOnKeyListener(new View.OnKeyListener() {
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                /*
-                 * Normally there would have to be a way to associate
-                 * a view with a GlkWindow; in this implementation there
-                 * is only one window to dispatch key events to.
-                 */
-                return glk.keyInput(v, keyCode, event);
-            }
-        });
+        tv = new SimpleTextBufferView(this);
+        glk = new ModelGlk(this, tv);
         
         tv.setFocusable(true);
         tv.setCursorVisible(false);
         setContentView(tv);
+        
+        startInterpreter();
     }
     
     private void startInterpreter() {
-        Thread terpThread = new Thread(new Runnable() {
+        terpThread = new Thread(new Runnable() {
            @Override
             public void run() {
                String[] args = new String[] {"model"};
