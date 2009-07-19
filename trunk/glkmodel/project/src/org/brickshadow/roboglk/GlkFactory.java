26ca501d863a4953957fd6a336eb932344c919ca
edward.mccardell <edward.mccardell@6169d07e-6ce4-11de-b5d2-8dea7b56f9cf>
2009-07-19 01:37:10 +0000

    temporary glkmodel breakage--removing stale roboglk

--- trunk/glkmodel/project/src/org/brickshadow/roboglk/GlkFactory.java
+++
@@ -1,9 +0,0 @@
-package org.brickshadow.roboglk;
-
-public class GlkFactory {
-    public static final String storyPath = null;
-
-    public static native boolean startup(Glk glk, String[] args);
-    
-    public static native int run();
-}
